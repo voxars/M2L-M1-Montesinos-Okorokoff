0d3661e284dedfd922e7f5b852eceb5e41ffb604
Rubylium <alexisoko0@gmail.com>
2020-03-19 11:09:59 +0100

    Update connectBdd.php

--- BDD/connectBdd.php
+++ BDD/connectBdd.php
@@ -11,4 +11,10 @@ catch(Exception $e)
         echo 'Une erreur de connexion à la BDD est survenue !';
 		die('Erreur : ' . $e->getMessage());
 }
+
+function nbLignesRequete($requete){
+    $req= $connexion->query($requete); // on va chercher tous les membres de la table qu'on trie par ordre croissant
+
+    return $req->rowcount();
+}
 ?>
